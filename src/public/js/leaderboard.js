1f20c9ec39ca8f81228d3c0200af54f3629eaa61
Ben Zobrist <ben@zobie.com>
2023-12-08 04:27:35 -0700

    Updated Footer

--- src/public/js/leaderboard.js
+++ src/public/js/leaderboard.js
@@ -55,9 +55,33 @@ function getPlayerName() {
   return localStorage.getItem('userName') ?? 'Guest';
 }
 
+async function getdadjoke() {
+  try {
+    const opt = { headers: { "Accept": "application/json" } };
+    const resp = await fetch('https://icanhazdadjoke.com/', opt);
+    if(resp.ok) {
+      
+      const json = await resp.json();
+      const { joke } = json;
+      setdadjoke(joke);
+      return;
+    }
+    throw new Error('Request failed!')
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+function setdadjoke(joke) {
+  const el = document.querySelector('#joke');
+  el.textContent = joke;
+}
+
 const tmp = document.querySelector('#loggedName');
 tmp.textContent = getPlayerName();
 
 loadScores();
+
+getdadjoke();
 
 
